c584693796e8ce3d4a070c07957661920ddb42a4
Kirill Nitchenko <Ollie4815@gmail.com>
2018-08-22 09:12:58 +0300

    2

--- weather.py
+++ weather.py
@@ -19,7 +19,7 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 api_humy = response.json()['main']['humidity']
 #print('The London\'s humidity on Today is:', api_humy)
 #send value in ES
-weather=es.index(index ='weather1',doc_type='weather_city',id=1,body=response)
+es.index(index ='weather1',doc_type='weather_city',id=1,body=response)
 #weather_out=es.get(index='weather1',doc_type='weather_city',id =1)
 #print(weather_out)
 
@@ -31,7 +31,7 @@ driver.get('https://openweathermap.org/city/2643743')
 humidity = driver.find_element_by_xpath("//td[text()='Humidity']/following-sibling::td")
 value_humidity = humidity.text
 #send value in ES
-sel_humidity = es.index(index ='weather1',doc_type='weather_city',id=2,body=value_humidity)
+es.index(index ='weather1',doc_type='weather_city',id=2,body=value_humidity)
 driver.quit()
 
 #assertion 2 values
